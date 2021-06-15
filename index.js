46418863dbc8205b2dc93d64d3f8a7c987d4a28b
Ryan Evans <rdevans87@gmail.com>
2021-06-15 04:06:31 -0400

    working on prompts and functions

--- index.js
+++ index.js
@@ -3,14 +3,48 @@ const Enginner = require("./lib/Engineer");
 const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const html = require("./src/htmlTemp");
+// const html = require("./src/htmlTemp");
 const util = require("util");
 const path = require("path");
 const fs = require("fs");
 
-const writeFileAsync = util.promisify(fs.writeFile)
-const appendFileAsync = util.promisfy(fs.appendFile)
+const writetoFile = util.promisify(fs.writeFile)
+const appendFile = util.promisfy(fs.appendFile)
 const sampleDirectory = path.resolve(_dirname, "sample")
 
 let employeeArr = []
 let teamArr = []
+
+async function mainMenu() {
+
+    try {
+
+
+    }
+
+}
+
+
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err) => {
+          console.log(err)
+  })
+  
+  };
+  
+  // Function call to initialize app
+  function init() {
+      inquirer.prompt(questions)
+      .then((responses) => {
+          console.log(responses)
+          const readMe = generateMarkdown(responses)
+          writeToFile('sample.html', sampleHTML)
+          console.log("Team Profile Generated")
+   
+      });
+  
+  };
+
+  init(;)
+  
+  
