a15d581d1251f9939da76be53c4030b0ac21804b
Ryan Evans <rdevans87@gmail.com>
2021-06-15 03:29:04 -0400

    working thru tests and index for development

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const Manager = require('./lib/Manager')
-
+const Manager = require("./lib/Manager");
+const Enginner = require("./lib/Engineer");
+const Enginner = require("./lib/Employee");
+const Enginner = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+
