532ac4f8f921220588eecfbad81f469cd410cb78
Ryan Evans <rdevans87@gmail.com>
2021-06-15 05:31:17 -0400

    first test created for intern

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,7 @@
 const Intern = require("../lib/Intern");
+
+test("get school from constructor", () => {
+    const testValue = "The Ohio State University";
+    const emp = new Intern("Ali", 4, "ali@fmail.com", testValue);
+});
+
