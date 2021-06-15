648e7e136ae424422aa7889ce11a89e648bc8488
Ryan Evans <rdevans87@gmail.com>
2021-06-15 04:16:24 -0400

    setting up user prompts

--- index.js
+++ index.js
@@ -3,19 +3,90 @@ const Enginner = require("./lib/Engineer");
 const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-// const html = require("./src/htmlTemp");
 const util = require("util");
 const path = require("path");
 const fs = require("fs");
 
 const writetoFile = util.promisify(fs.writeFile)
 const appendFile = util.promisfy(fs.appendFile)
-const sampleDirectory = path.resolve(_dirname, "sample")
+const samplePath = path.joinh(sampleDir, "sample.html")
+const sampleDir = path.resolve(_dirname, "sample")
+const html = require("./src/htmlTemp");
 
 let employeeArr = []
 let teamArr = []
 
+
+const userPrompts = [
+    {
+
+        type:
+        name:
+        message:
+
+    }
+
+    {
+
+        type:
+        name:
+        message:
+    
+    }
+
+    {
+
+        type:
+        name:
+        message:
+        
+     }
+    {
+
+        type:
+        name:
+        message:
+            
+    }
+     {
+
+        type:
+        name:
+        message:
+                
+    }
+    {
+
+        type:
+        name:
+        message:
+                    
+    }
+    {
+
+        type:
+        name:
+        message:
+                        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
 async function mainMenu() {
+    console.log
 
     try {
 
@@ -27,24 +98,23 @@ async function mainMenu() {
 
 function writeToFile(fileName, data) {
     return fs.writeFile(fileName, data, (err) => {
-          console.log(err)
-  })
-  
-  };
-  
-  // Function call to initialize app
-  function init() {
-      inquirer.prompt(questions)
-      .then((responses) => {
-          console.log(responses)
-          const readMe = generateMarkdown(responses)
-          writeToFile('sample.html', sampleHTML)
-          console.log("Team Profile Generated")
-   
-      });
-  
-  };
+        console.log(err)
+    })
 
-  init(;)
-  
-  
+};
+
+// Function call to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((responses) => {
+            console.log(responses)
+            const readMe = generateMarkdown(responses)
+            writeToFile('sample.html', sampleHTML)
+            console.log("Team Profile Generated")
+
+        });
+
+};
+
+init(;)
+
