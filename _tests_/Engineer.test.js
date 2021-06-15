5fb8e5d287b9661612180586a9906a1caf451715
Ryan Evans <rdevans87@gmail.com>
2021-06-15 05:38:30 -0400

    first engineer test created

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,1 +1,8 @@
 const Engineer = require("../lib/Engineer")
+
+test("Set GitHub Account from constructor", () => {
+    const testValue = "GitHub"
+    const emp = new Engineer("Bobby", 2, "https://github.com/fgithubaccount", testValue)Engineer;
+    expect(emp.github).toBe(testValue);
+
+});
