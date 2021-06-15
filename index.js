1f592be7db899af7b89911258e071e6076f5ebee
Ryan Evans <rdevans87@gmail.com>
2021-06-15 03:48:25 -0400

    adding dependencies and required files

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const Manager = require("./lib/Manager");
 const Enginner = require("./lib/Engineer");
-const Enginner = require("./lib/Employee");
-const Enginner = require("./lib/Intern");
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const html = require("./src/htmlTemp");
+const util = require("util");
+const path = require("path");
 const fs = require("fs");
 
+const writeFileAsync = util.promisify(fs.writeFile)
+const appendFileAsync = util.promisfy(fs.appendFile)
+const sampleDirectory = path.resolve(_dirname, "sample")
 
+let employeeArr = []
+let teamArr = []
