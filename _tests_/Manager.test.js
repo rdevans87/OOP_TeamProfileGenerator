e1aebe94ff9d873e59e45781f405d58d0b2fffbc
Ryan Evans <rdevans87@gmail.com>
2021-06-15 05:41:30 -0400

    first manager test created

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,2 +1,10 @@
 const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee")
+// const Employee = require("../lib/Employee")
+
+test("Set Office Number from constructor argument", () => {
+const testValue = 1;
+const emp = new Manager("Johnny", 1, "johnny@fmail.com", testValue);
+expect(emp.officeNumber).toBe(testValue);
+
+
+});
